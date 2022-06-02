495f4564c7174dfce201054691ce79cae67c9807
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-06-02 16:55:47 +0200

    ADD : CR in db

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +16,11 @@ class WishController extends AbstractController {
     /**
      * @Route("/list",name="app_list")
      */
-    public function list():Response{
-        return $this->render("wish/list.html.twig");
+    public function list(WishRepository $WishRepo):Response{
+
+        $wishList = $WishRepo->findAll();
+
+        return $this->render("wish/list.html.twig", ["wishList" => $wishList]);
     }
 
 
@@ -23,5 +31,29 @@ class WishController extends AbstractController {
         return $this->render("wish/detail.html.twig");
     }
 
+    /**
+     * @Route("/add", name="app_add")
+     */
+    public function add(EntityManagerInterface $em): Response
+    {
+        $w = new Wish();
+        $w->setTitle("Souhait 1");
+        $w->setDescription("Blablablabla");
+        $w->setAuthor("Auteur1");
+        $em->persist($w);
+        $em->flush();
+
+        return $this->render('wish/list.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+/**
+     * @Route("/afficher", name="app_display")
+     */
+    public function afficher(WishRepository $WishRepo): Response
+    {
+        dd($WishRepo->findAll());
+    }
 
 }
